a31d5dbe2a71f942636885c3533a5286471a000b
JCano <jjcadu@gmail.com>
2018-09-04 18:57:37 -0500

    Mejoras al diseño de la pantalla principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,58 @@
 	<div class="row justify-content-center">
 		<div class="col-md-8">
 			<div class="card">
-				<div class="card-header border-success">
-					Domingo 2 de Noviembre
-					<a href="#" class="float-right badge badge-pill badge-primary">+</a>
+				<div class="card-header text-white bg-primary">
+					<div class="row">
+						<div class="col-8 text-gray">
+							Domingo 2 de Noviembre
+						</div>
+						<div class="col-4 text-right">
+							<i class="far fa-plus-square text-white"></i>
+						</div>
+					</div>
 				</div>
 				<ul class="list-group list-group-flush">
 					<li class="list-group-item">
-						Venta
-						<small class="text-muted"> - Ingresos</small>
-						<a href="#" class="float-right badge badge-pill badge-success">$40.000</a>
+						<div class="row">
+							<div class="col-8">
+								Venta
+								<small class="text-muted"> - Ingresos</small>
+							</div>
+							<div class="col-4 text-right">
+								<span class="badge badge-pill badge-success">$40000.000</span>
+							</div>
+						</div>
 					</li>
 					<li class="list-group-item">
-						Uber
-						<small class="text-muted"> - Transporte</small>
-						<span class="badge badge-pill badge-info float-right text-white">$7.000</span>
+						<div class="row">
+							<div class="col-8">
+								Uber
+								<small class="text-muted"> - Transporte</small>
+							</div>
+							<div class="col-4 text-right">
+								<span class="badge badge-pill badge-info text-white">$7.000</span>
+							</div>
+						</div>
 					</li>
 					<li class="list-group-item">
-						Almuerzo
-						<small class="text-muted"> - Alimentación</small>
-						<span class="badge badge-pill badge-info float-right text-white">$15.000</span>
+						<div class="row">
+							<div class="col-8">
+							Almuerzo
+							<small class="text-muted"> - Alimentación</small>
+							</div>
+							<div class="col-4 text-right">
+								<span class="badge badge-pill badge-info text-white">$1'555.000</span>
+								<i class="far fa-times-circle text-danger"></i>
+								<i class="far fa-check-circle text-primary"></i>
+							</div>
+						</div>
 					</li>
 				</ul>
 			</div>
 			<div class="card mt-3">
 				<div class="card-header alert-success">
 					Saldo al empezar el día
-						<span class="float-right">$15.000</span>
+					<span class="float-right">$15.000</span>
 				</div>
 				<div class="card-header alert-success">
 					Saldo al terminar el día
