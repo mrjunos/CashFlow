cf4810ab5b749852a27e205814a5cc33cd48ebf4
Juanjo <jjcadu@hotmail.com>
2018-09-04 22:11:03 -0500

    Mejoras en la interfáz del home y creación del formulario de nueva actividad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,9 @@
 							Domingo 2 de Noviembre
 						</div>
 						<div class="col-4 text-right">
-							<i class="far fa-plus-square text-white"></i>
+							<button type="button" class="btn btn-sm btn-light" data-toggle="modal" data-target="#exampleModal">
+								<i class="fas fa-plus text-primary"></i>
+							</button>
 						</div>
 					</div>
 				</div>
@@ -20,10 +22,10 @@
 						<div class="row">
 							<div class="col-8">
 								Venta
-								<small class="text-muted"> - Ingresos</small>
+								<small class="text-muted">Ingresos</small>
 							</div>
 							<div class="col-4 text-right">
-								<span class="badge badge-pill badge-success">$40000.000</span>
+								<small class="text-success">$40.000</small>
 							</div>
 						</div>
 					</li>
@@ -31,23 +33,23 @@
 						<div class="row">
 							<div class="col-8">
 								Uber
-								<small class="text-muted"> - Transporte</small>
+								<small class="text-muted">Transporte</small>
 							</div>
 							<div class="col-4 text-right">
-								<span class="badge badge-pill badge-info text-white">$7.000</span>
+								<small class="text-primary">$7.000</small>
 							</div>
 						</div>
 					</li>
 					<li class="list-group-item">
 						<div class="row">
-							<div class="col-8">
-							Almuerzo
-							<small class="text-muted"> - Alimentación</small>
+							<div class="col col-xs-7 col-sm-8">
+								Almuerzo
+								<small class="text-muted">Alimentación</small>
 							</div>
-							<div class="col-4 text-right">
-								<span class="badge badge-pill badge-info text-white">$1'555.000</span>
-								<i class="far fa-times-circle text-danger"></i>
-								<i class="far fa-check-circle text-primary"></i>
+							<div class="col col-xs-5 col-sm-4 text-right">
+								<small class="text-primary">$99'999.999</small>
+								<a href=""><i class="far fa-times-circle text-danger"></i></a>
+								<a href=""><i class="far fa-check-circle text-primary"></i></a>
 							</div>
 						</div>
 					</li>
@@ -60,10 +62,57 @@
 				</div>
 				<div class="card-header alert-success">
 					Saldo al terminar el día
-						<span class="float-right">$23.000</span>
+					<span class="float-right">$23.000</span>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<form>
+				<div class="modal-body">
+					<div class="form-group">
+						<small id="emailHelp" class="form-text text-muted">Categoría</small>
+						<select class="form-control form-control-sm" id="exampleFormControlSelect1">
+							<option>Transporte</option>
+							<option>Alimentación</option>
+							<option>Salud</option>
+							<option>Diversión</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<small id="emailHelp" class="form-text text-muted">Subcategoría</small>
+						<select class="form-control form-control-sm" id="exampleFormControlSelect1">
+							<option>Bus</option>
+							<option>Metro / Cívica</option>
+							<option>Taxi / Uber</option>
+							<option>Otros transportes</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<small id="emailHelp" class="form-text text-muted">Comentario</small>
+						<textarea class="form-control form-control-sm" id="exampleFormControlTextarea1" rows="2" placeholder="Comentario"></textarea>
+					</div>
+					<div class="form-group">
+						<small id="emailHelp" class="form-text text-muted">Fecha</small>
+						<input type="date" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Fecha">
+					</div>
+					<div class="form-group">
+						<small id="emailHelp" class="form-text text-muted">Valor</small>
+						<input type="number" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Valor">
+					</div>
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+					<button type="submit" class="btn btn-primary float-right">Crear</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+
 @endsection
