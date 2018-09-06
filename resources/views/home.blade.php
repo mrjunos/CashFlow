65512b2302080ef6fb54cf48ba42b2028971417a
Juanjo <jjcadu@hotmail.com>
2018-09-05 23:02:43 -0500

    Mejoras en la interfáz y en el formulario de creación

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,20 @@
 <div class="container">
 	<div class="row justify-content-center">
 		<div class="col-md-8">
+			<div class="row mb-3">
+				<div class="col">
+					<button type="button" class="btn btn-info text-white">
+						<i class="fas fa-angle-left text-white"></i>
+						Sábado
+					</button>
+				</div>
+				<div class="col text-right">
+					<button type="button" class="btn btn-info text-white">
+						Lunes
+						<i class="fas fa-angle-right text-white"></i>
+					</button>
+				</div>
+			</div>
 			<div class="card">
 				<div class="card-header text-white bg-primary">
 					<div class="row">
@@ -18,6 +32,10 @@
 					</div>
 				</div>
 				<ul class="list-group list-group-flush">
+					<li class="list-group-item list-group-item-primary">
+						Saldo al empezar el día
+						<span class="float-right">$15.000</span>
+					</li>
 					<li class="list-group-item">
 						<div class="row">
 							<div class="col-8 border-right">
@@ -53,17 +71,11 @@
 							</div>
 						</div>
 					</li>
+					<li class="list-group-item list-group-item-primary">
+						Saldo al terminar el día
+						<span class="float-right">$33.000</span>
+					</li>
 				</ul>
-			</div>
-			<div class="card mt-3">
-				<div class="card-header alert-success">
-					Saldo al empezar el día
-					<span class="float-right">$15.000</span>
-				</div>
-				<div class="card-header alert-success">
-					Saldo al terminar el día
-					<span class="float-right">$23.000</span>
-				</div>
 			</div>
 		</div>
 	</div>
@@ -74,16 +86,25 @@
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
 	<div class="modal-dialog" role="document">
 		<div class="modal-content">
-			<form>
-				<div class="modal-body">
-					<div class="form-group">
-						<small id="emailHelp" class="form-text text-muted">Categoría</small>
-						<select class="form-control form-control-sm" id="exampleFormControlSelect1">
-							<option>Transporte</option>
-							<option>Alimentación</option>
-							<option>Salud</option>
-							<option>Diversión</option>
-						</select>
+			<div class="modal-body">
+				<form>
+					<div class="form-row">
+						<div class="form-group col-6">
+							<small class="form-text text-muted">Tipo</small>
+							<select class="form-control form-control-sm" id="exampleFormControlSelect1">
+								<option>Egreso</option>
+								<option>Ingreso</option>
+							</select>
+						</div>
+						<div class="form-group col-6">
+							<small id="emailHelp" class="form-text text-muted">Categoría</small>
+							<select class="form-control form-control-sm" id="exampleFormControlSelect1">
+								<option>Transporte</option>
+								<option>Alimentación</option>
+								<option>Salud</option>
+								<option>Diversión</option>
+							</select>
+						</div>
 					</div>
 					<div class="form-group">
 						<small id="emailHelp" class="form-text text-muted">Subcategoría</small>
@@ -94,17 +115,19 @@
 							<option>Otros transportes</option>
 						</select>
 					</div>
+					<div class="form-row">
+						<div class="form-group col-6">
+							<small id="emailHelp" class="form-text text-muted">Fecha</small>
+							<input type="date" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Fecha">
+						</div>
+						<div class="form-group col-6">
+							<small id="emailHelp" class="form-text text-muted">Valor</small>
+							<input type="number" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Valor">
+						</div>
+					</div>
 					<div class="form-group">
 						<small id="emailHelp" class="form-text text-muted">Comentario</small>
 						<textarea class="form-control form-control-sm" id="exampleFormControlTextarea1" rows="2" placeholder="Comentario"></textarea>
-					</div>
-					<div class="form-group">
-						<small id="emailHelp" class="form-text text-muted">Fecha</small>
-						<input type="date" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Fecha">
-					</div>
-					<div class="form-group">
-						<small id="emailHelp" class="form-text text-muted">Valor</small>
-						<input type="number" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Valor">
 					</div>
 					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
 					<button type="submit" class="btn btn-primary float-right">Crear</button>
