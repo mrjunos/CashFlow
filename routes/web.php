09d11ae21311e94397eee4ac587fe223e776b54b
JCano <jjcadu@gmail.com>
2018-09-21 17:59:33 -0500

    Preparando la creación de Actividades y la vista de semana

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,4 @@ Route::view('/', 'welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@home')->name('home');
-Route::get('/home/{date}', 'HomeController@day')->name('home');
+Route::get('/home/{date}', 'HomeController@day')->name('home/');
